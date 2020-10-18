5c6cd8a1d6123ae8c1a07e4003d8b6b463ee2994
shi-ivam <rockyshivammm67@gmail.com>
2020-10-19 01:51:40 +0530

    Initial and Basic Styling

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Style.module.css';
 
 export default function Home() {
   return (
@@ -8,58 +8,73 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      <div className={styles.header}>
+        <h1 className={styles.pageTitle}>Blobg</h1>
+      </div>
+      <div className={styles.homeMain}>
+        <div className={styles.homeMainCh}> 
+          <div className={styles.homeMainChMainPostImage}>
+            <img className={styles.fullWidthImage} src="/firstpost.jpg"></img>
+          </div>
+          <div className={styles.mainChInfo}>
+            <h3 className={styles.mainChInfoTitle}>Top Places to Visit This Holiday Season</h3>
+            <p className={styles.mainChInfoExtract}>As winter is around the corner, here's our top pick of places to visit in India this season. Auli, Uttarakhand. Facebook. Add. Nanital, Uttarakhand. Zoo. Add. Binsar, Uttarakhand. Facebook. Add. Manali, Himachal Pradesh. Facebook. Add. Shimla, Himachal Pradesh. Facebook
             </p>
-          </a>
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={styles.homeMainCh}>
+          <div className={styles.innerSecCh}>
+            <div className={styles.innerSecChStory}>
+              <div className={styles.innerSecChStoryImageWrap}>
+              <img className={styles.secStoryImg} src="/firstpost.jpg"></img>
+              </div>
+              <div className={styles.SecChStoryInfo}>
+                <h1 className={styles.secStoryTitle}>
+                  Best Places to Visit
+                </h1>
+                <p className={styles.secStoryExtract}>As winter is around the corner, here's our top pick of places to visit in India this season.
+                </p>
+              </div>
+            </div>
+            <div className={styles.innerSecChStory}>
+              <div className={styles.innerSecChStoryImageWrap}>
+              <img className={styles.secStoryImg} src="/firstpost.jpg"></img>
+              </div>
+              <div className={styles.SecChStoryInfo}>
+                <h1 className={styles.secStoryTitle}>
+                  Best Places to Visit
+                </h1>
+                <p className={styles.secStoryExtract}>As winter is around the corner, here's our top pick of places to visit in India this season.
+                </p>
+              </div>
+            </div>
+            <div className={styles.innerSecChStory}>
+              <div className={styles.innerSecChStoryImageWrap}>
+              <img className={styles.secStoryImg} src="/firstpost.jpg"></img>
+              </div>
+              <div className={styles.SecChStoryInfo}>
+                <h1 className={styles.secStoryTitle}>
+                  Best Places to Visit
+                </h1>
+                <p className={styles.secStoryExtract}>As winter is around the corner, here's our top pick of places to visit in India this season.
+                </p>
+              </div>
+            </div>
+            <div className={styles.innerSecChStory}>
+              <div className={styles.innerSecChStoryImageWrap}>
+              <img className={styles.secStoryImg} src="/firstpost.jpg"></img>
+              </div>
+              <div className={styles.SecChStoryInfo}>
+                <h1 className={styles.secStoryTitle}>
+                  Best Places to Visit
+                </h1>
+                <p className={styles.secStoryExtract}>As winter is around the corner, here's our top pick of places to visit in India this season.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
